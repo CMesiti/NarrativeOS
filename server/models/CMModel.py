534f210fa178e5076248f39e06a9b5550c750ebb
Caleb Mesiti <105940692+CMesiti@users.noreply.github.com>
2026-01-29 17:48:40 -0500

    fix enum type mapping

--- server/models/CMModel.py
+++ server/models/CMModel.py
@@ -4,10 +4,15 @@ from sqlalchemy.orm import mapped_column, Mapped, relationship
 from sqlalchemy import ForeignKey
 from datetime import datetime
 import uuid
+import enum
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from models import Campaigns, Users
+class Role(enum.Enum):
+    DM = "DM"
+    PLAYER = "Player"
+    VIEWER = "VIEWER"
 
 #classic many to many association table
 class CampaignMembers(ModelBase):
@@ -16,7 +21,7 @@ class CampaignMembers(ModelBase):
     #declare table columns, dtypes, and mappings
     campaign_id:Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("campaigns.campaign_id", ondelete="CASCADE"), primary_key=True)
     user_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True),ForeignKey("users.user_id", ondelete="CASCADE"), primary_key = True)
-    user_role: Mapped[dict] = mapped_column(ENUM(('DM', 'Player', 'Viewer'), name = "USER_ROLE"))
+    user_role: Mapped[Role] = mapped_column(ENUM(Role, name = "USER_ROLE"))
     joined_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True))
 
     #many to 1

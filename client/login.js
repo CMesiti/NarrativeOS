c43f24028bb7851c74e17332f1fd3fde66f8050d
Caleb Mesiti <105940692+CMesiti@users.noreply.github.com>
2026-02-13 16:31:40 -0500

    front-end

--- client/login.js
+++ client/login.js
@@ -1,5 +1,9 @@
 function login(){
-    let email = document.login.email.value;
-    let password = document.login.password.value;
+    const form = document.getElementById("login")
+    form.addEventListener("submit", (event)=>{
+        console.log(event)
+    })
+    let email = document.getElementById("email")
+    let password = document.getElementById("password")
     console.log(email, password)
 }

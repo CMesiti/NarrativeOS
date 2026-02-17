da51e3ba41e6821e372be072e911d7e00b64fe2c
Caleb Mesiti <calebmesiti555@gmail.com>
2026-02-17 12:29:07 -0500

    test files

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+#initial test authentication request login
+class AuthActions(obj):
+    def __init__(self):
+        pass
+    def login(self, username, password):
+        #post request to the login
+        #requires client driver
+        pass
+
--- tests/user_tests.py
+++
@@ -1,7 +0,0 @@
-"""
-Key areas of code requiring testing include critical business logic (via unit tests), 
-data validation and boundary conditions (highest/lowest values, nulls), 
-database operations and service interactions (integration tests), 
-user interface workflows (functional tests), 
-and security/error handling paths. 
-"""
